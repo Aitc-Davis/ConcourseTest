7a37caca1b463121a0ad46876829562ffc266f91
Davis <davis@aitc.com.tw>
2020-07-29 11:33:06 +0800

    Update

--- docker-file/consourse/build.sh
+++
@@ -1,5 +0,0 @@
-docker rmi vm.docker.internal:5000/concourse/registry-image-resource:by-davis
-
-docker build . -t vm.docker.internal:5000/concourse/registry-image-resource:by-davis
-
-docker push vm.docker.internal:5000/concourse/registry-image-resource:by-davis
